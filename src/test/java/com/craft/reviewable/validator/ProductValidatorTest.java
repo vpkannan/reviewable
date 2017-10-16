b9253efdfda50accf0db4ef3603a106472a5874a
Vignesh Kannan <vignesh.p.kannan@gmail.com>
2017-10-16 00:12:33 -0500

    add unit tests for Product and Review validator

--- src/test/java/com/craft/reviewable/validator/ProductValidatorTest.java
+++ src/test/java/com/craft/reviewable/validator/ProductValidatorTest.java
@@ -9,9 +9,16 @@ import com.craft.reviewable.domain.Product;
 public class ProductValidatorTest {
 
 	@Test
+	public void testSupports() {
+
+		boolean result = new ProductValidator().supports(Product.class);
+		assert (result);
+	}
+
+	@Test
 	public void testValidate() {
 
-		Product product = new Product("prod1", "", 3.0, 1, 1, 1, 1, 1);
+		Product product = new Product("prod1", "", 3.0, 0, 0, 0, 0, 1);
 		Errors errors = new BeanPropertyBindingResult(product, "product");
 
 		ProductValidator validator = new ProductValidator();
---
+++ src/test/java/com/craft/reviewable/validator/ReviewValidatorTest.java
@@ -0,0 +1,30 @@
+package com.craft.reviewable.validator;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+
+import com.craft.reviewable.domain.Review;
+
+public class ReviewValidatorTest {
+
+	@Test
+	public void testSupports() {
+
+		boolean result = new ReviewValidator().supports(Review.class);
+		assert (result);
+	}
+
+	@Test
+	public void testValidate() {
+
+		Review review = new Review("review1", "", 8, "Good product", "Excellent design", "", new Date());
+		Errors errors = new BeanPropertyBindingResult(review, "review");
+
+		ReviewValidator validator = new ReviewValidator();
+		validator.validate(review, errors);
+
+	}
+}
